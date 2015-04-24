4a8e0c31fb5c2bb6e06ef81e8339cc5c874f92ac
Melissa <mactechy@gmail.com>
2015-04-23 20:27:05 -0400

    first commit to github for this project.

---
+++ index.php
@@ -0,0 +1,80 @@
+
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css" type="text/css" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+           <!--[if lt IE 9]>
+                 <link rel="stylesheet" href="css/ie8.css">
+        <![endif]-->
+    </head>
+    <body class="body">
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+
+    
+    <header class="mainHeader">
+    
+        <nav>
+
+        
+      
+    
+<img src="img/bamboo-logo.png">
+    <ul>
+     <li><a href="#">Home</a></li>
+       <li><a href="#">About</a></li>
+    <li><a href="#">Nature Preserve</a></li>
+    <li><a href="#">Tours</a></li>
+    <li><a href="#">Hours</a></li>
+    <li><a href="#">Contact Us</a></li>
+                    
+        </ul></nav>
+    </header>
+        
+    <div class="mainContent">
+        <div class="content">   
+                <article class="topcontent">    
+                    <header>
+                        <h2>DISCOVER BAMBOO TRAIL</h2>
+                    </header>
+                    
+                    
+                    <content>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        
+                    </content>
+                    
+                </article>
+
+
+
+        </div>
+            
+    </div>
+  
+    
+
+</body>
+</html>
